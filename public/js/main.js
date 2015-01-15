0768f46f7cd8b7573b61aeae8549d66605d026d2
Jared Darr <jared@Jareds-MacBook-Pro.local>
2015-01-14 20:54:15 -0800

    Exercise 3 - jQuery Entry Level

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,10 @@
+//handle links output
+$(document).ready(function(){
 
+	var links = $('.header-container a');
+	links.on('click',function(e){
+		e.preventDefault();
+		console.log($(this).prop('href'));
+	});
+
+});
