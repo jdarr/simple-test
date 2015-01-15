7976bcc6940c9d96073f2f503a5732841468dc7d
Jared Darr <jared@Jareds-MacBook-Pro.local>
2015-01-14 21:32:30 -0800

    Exercise 4 – jQuery Entry Level

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# simple-test
-Simple Test
+# Simple Test
+
+## Base Setup
+
+I believe selecting the right tools for the job is important, and since this test is fairly simple, I used a minimal set of tools for the site.  I included HTML5 boiler plate as a starting point, and then hand coded everything else. I choose not in include a buid system or grid framework as I felt this would overcomplicate the task at hand.
+
+### Note
+Be sure to set this project up as a website, and don't just run it off the file system to avoid errors when loading in the json.
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,43 @@
-//handle links output
+
 $(document).ready(function(){
 
+	//handle links output
 	var links = $('.header-container a');
 	links.on('click',function(e){
 		e.preventDefault();
 		console.log($(this).prop('href'));
 	});
 
+	//handle ajax request of data
+	$.ajax({
+		url:'json/data.json',
+		dataType:'json',
+		success:function(data, status, xhr){
+			
+			//display data to console
+			console.log(data);
+
+			//loop through json
+			var thumbs = data.thumbs;
+			var html = '';
+			for(var i=0;i<thumbs.length;i++){
+				html += thumbHtml( thumbs[i].title, thumbs[i].thumb, thumbs[i].url);
+			}
+
+			//render html in dom
+			$('.thumb-wrapper').html( html );
+
+		}
+	});             
+
 });
+
+var thumbHtml = function( title, thumb, url){
+	
+	var str = "<div class='thumb-container'>";
+		str += "<a href='"+ url + "'><img src='" + thumb + "' alt='" + title + "' /></a>";
+		str += "<span class='caption'>" + title + "</span>";
+		str += "</div>";
+
+	return str;
+}
